c915b7bdfdc7ba6fc17adcca8d5fc77b3c431727
Rehan Hanar <rehanhanar@Rehans-MacBook-Air.local>
2019-07-20 13:32:54 +0530

    404 page added

---
+++ wp-content/plugins/404page/inc/admin/404page-admin-page.php
@@ -0,0 +1,7 @@
+<div class="wrap pp-admin-page-wrapper" id="pp-404page-settings">
+  <h1>
+    <span><br />
+<b>Fatal error</b>:  Uncaught Error: Call to undefined function esc_html() in /Applications/XAMPP/xamppfiles/htdocs/manapparai/public_html/wp-content/plugins/404page/inc/admin/404page-admin-page.php:3
+Stack trace:
+#0 {main}
+  thrown in <b>/Applications/XAMPP/xamppfiles/htdocs/manapparai/public_html/wp-content/plugins/404page/inc/admin/404page-admin-page.php</b> on line <b>3</b><br />
